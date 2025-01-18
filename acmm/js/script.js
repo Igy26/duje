5e006afef62ea744097b8948754e83561dc720c5
Igy26 <165638413+Igy26@users.noreply.github.com>
2025-01-18 16:55:34 +0800

    Add files via upload

---
+++ acmm/js/script.js
@@ -0,0 +1,5 @@
+// Example of Search Button Functionality (if needed)
+document.querySelector('.search-bar button').addEventListener('click', function() {
+    const query = document.querySelector('.search-bar input').value;
+    alert("You searched for: " + query);
+});
